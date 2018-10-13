5f72d7cb9a214d8fabde19cf81b61b30dc92fabe
MrLuki75 <lpazik@icloud.com>
2018-10-13 09:36:22 +0900

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lukasz Pazik on 13/10/2018.
+//  Copyright © 2018 Lukasz Pazik. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
